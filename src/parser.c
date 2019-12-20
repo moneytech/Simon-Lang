3318944951b4e3e4923b6dbe82b302ac6b1cad97
Simon Risberg <simon.risberg@abbindustrigymnasium.se>
2019-12-20 23:31:13 +0100

    add things

--- src/parser.c
+++ src/parser.c
@@ -1,16 +1,35 @@
 #include "include/parser.h"
-
+#include <stdio.h>
 
 parser_T* init_parser(lexer_T* lexer)
 {
+    parser_T* parser = calloc(1, sizeof(struct PARSER_STRUCT));
+    parser->lexer = lexer;
+    parser->current_token = lexer_get_next_token(lexer);
+
+    return parser;
 }
 
 void parser_eat(parser_T* parser, int token_type)
 {
+    if (parser->current_token->type == token_type)
+    {
+        parser->current_token = lexer_get_next_token(parser->lexer);
+    }
+    else
+    {
+        printf(
+            "Unexpexted token `%s`, with type %d",
+            parser->current_token->value,
+            parser->current_token->type
+        );
+        exit(1);
+    }
 }
 
 AST_T* parser_parse(parser_T* parser)
 {
+    return parser_parse_statements();
 }
 
 AST_T* parser_parse_statement(parser_T* parser)
@@ -19,6 +38,24 @@ AST_T* parser_parse_statement(parser_T* parser)
 
 AST_T* parser_parse_statements(parser_T* parser)
 {
+    AST_T* compound = init_ast(AST_COMPOUND);
+    compound->compound_value = calloc(1, sizeof(struct AST_struct*));
+
+    AST_T* ast_statement = parser_parse_statement(parser);
+    compound->compound_value[0] = ast_statement;
+    compound->compound_size += 1;
+    
+
+    while (parser->current_token->type == TOKEN_SEMI)
+    {
+        parser_eat(parser, TOKEN_SEMI);
+
+        AST_T* ast_statement = parser_parse_statement(parser);
+        compound->compound_value[0] = ast_statement;
+        compound->compound_size += 1;
+    }
+
+    return compound;
 }
 
 AST_T* parser_parse_expr(parser_T* parser)
